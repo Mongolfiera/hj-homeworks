712a217e278c57a3d79eb49e522be785b6e7ad67
Mongolfiera <elenadruganova@mail.ru>
2018-04-27 23:48:04 +0300

    Update script.js

--- html-element-collection/drum-machine/js/script.js
+++ html-element-collection/drum-machine/js/script.js
@@ -1,8 +1,8 @@
 const drums = document.getElementsByClassName('drum-kit__drum');
 for (const drum of drums) {
   drum.onclick = () => {
-    const sound = drum.getElementsByTagName('audio');
-    sound[0].currentTime = 0;
-    sound[0].play();
+    const sound = drum.getElementsByTagName('audio')[0];
+    sound.currentTime = 0;
+    sound.play();
  }
 }
