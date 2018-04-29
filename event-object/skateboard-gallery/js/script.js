ea42361453f5cf8042891e297278842368da65db
Mongolfiera <elenadruganova@mail.ru>
2018-04-30 02:09:29 +0300

    Update script.js

--- event-object/skateboard-gallery/js/script.js
+++ event-object/skateboard-gallery/js/script.js
@@ -2,13 +2,13 @@ const galleryView = document.getElementById('view');
 const galleryNav = document.getElementById('nav');
 const images = galleryNav.getElementsByTagName('a');
 
-for (image of images) {
+for (const image of images) {
   image.addEventListener('click', displayImg);
 }
 
 function displayImg(event) {
   event.preventDefault();
-  for (image of images) {
+  for (const image of images) {
   	image.classList.remove('gallery-current');
   }
   galleryView.src = this.href;
