aed03d4e77927e7ee4e1482b752da45e0daddc0e
Mongolfiera <elenadruganova@mail.ru>
2018-05-02 01:04:32 +0300

    Update script.js

--- dom/contact-list/js/script.js
+++ dom/contact-list/js/script.js
@@ -5,7 +5,7 @@ const contactsList = document.getElementsByClassName('contacts-list')[0];
 let output = '';
 
 for (const contact of contacts) {
-  output += '\n<li data-email=\"'+ contact.email + '\" data-phone=\"' + contact.phone + '\">;\n\t<strong>' + contact.name + '</strong>;\n</li>';
+  output += `\n<li data-email=\"${contact.email}\" data-phone=\"${contact.phone}\">\n\t<strong>${contact.name}</strong>\n</li>`;
 }
 
 contactsList.innerHTML = output;
