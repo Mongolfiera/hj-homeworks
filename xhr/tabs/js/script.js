41d6676a9cdbd54d9c54d8878447e3a6b56cfc1c
Mongolfiera <elenadruganova@mail.ru>
2018-05-07 00:34:48 +0300

    Create script.js

---
+++ xhr/tabs/js/script.js
@@ -0,0 +1,41 @@
+'use strict'
+const tabs = document.querySelectorAll('.tabs a');
+const content = document.querySelector('#content');
+const preloader = document.querySelector('#preloader');
+updateContent(document.querySelector('.tabs a.active'));
+
+for (const tab of tabs) {
+  tab.addEventListener('click', switchTabs);
+}
+
+function switchTabs(event) {
+  event.preventDefault();
+  if (this.classList.contains('active')) {
+    return;
+  }
+  for (const tab of tabs) {
+  	tab.classList.remove('active');
+  }
+  this.classList.add('active');
+  updateContent(this.href);
+}
+
+function updateContent(link) {
+  const request = new XMLHttpRequest();
+  request.addEventListener('loadstart', onLoadStart);
+  request.addEventListener('load', onLoad);
+  request.open('GET', link);
+  request.send();
+
+  function onLoad() {
+    if (request.status === 200) {
+      preloader.classList.add('hidden');
+  	  content.innerHTML = request.responseText;
+    }	
+  }
+
+  function onLoadStart() {
+    content.innerHTML = '';
+    preloader.classList.remove('hidden');
+  }
+}
