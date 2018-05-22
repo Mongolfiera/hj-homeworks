f752691b363ea7d00433f258e1ec470d5e24d845
Mongolfiera <elenadruganova@mail.ru>
2018-05-23 01:51:27 +0300

    Update sort.js

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,10 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.classList.contains('prop__name')) {
+  	const fieldToSort = event.target.dataset.propName;
+    event.target.dataset.dir = (!event.target.dataset.dir) ? 1 : event.target.dataset.dir * -1;
+    event.currentTarget.dataset.sortBy = fieldToSort;
+    sortTable(fieldToSort, event.target.dataset.dir);
+  }    
 }
