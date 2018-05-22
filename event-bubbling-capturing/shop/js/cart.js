dbacc6140a41e082788e8d46c4c49756a3104d43
Mongolfiera <elenadruganova@mail.ru>
2018-05-23 01:18:03 +0300

    Update cart.js

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const itemsList = document.getElementsByClassName('items-list')[0];
+itemsList.addEventListener('click', handler);
+
+function handler(event) {
+	if(event.target.classList.contains('add-to-cart')) {
+		event.preventDefault();
+		addToCart(event.target.dataset);
+	}
+}
