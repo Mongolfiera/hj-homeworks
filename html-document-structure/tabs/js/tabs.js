979cc63976c2982958ef56be3c73fefb643ac947
Mongolfiera <elenadruganova@mail.ru>
2018-05-29 08:09:53 +0300

    Update tabs.js

---
+++ html-document-structure/tabs/js/tabs.js
@@ -0,0 +1,36 @@
+'use strict';
+const tabs = document.getElementById('tabs');
+const tabsContent = tabs.querySelector('.tabs-content');
+const tabsNav = tabs.querySelector('.tabs-nav');
+const articles = tabsContent.children;
+const tab = tabsNav.firstElementChild;
+
+for (const article of articles) {
+  const newTab = tab.cloneNode(true);
+  newTab.querySelector('a').textContent = article.dataset.tabTitle;
+  newTab.querySelector('a').className = article.dataset.tabIcon;
+  tabsNav.appendChild(newTab);
+  article.classList.add('hidden');
+}
+
+tabsNav.removeChild(tab);
+const tabsList = tabsNav.children;
+tabsList[0].classList.add('ui-tabs-active');
+articles[0].classList.remove('hidden');
+
+for (const tab of tabsList) {
+  tab.addEventListener('click', openArticle);
+}
+
+function openArticle() {
+  for (const tab of tabsList) {
+  	tab.classList.remove('ui-tabs-active');
+  }
+  for (const article of articles) {
+  	article.classList.add('hidden');
+  	if (article.dataset.tabTitle === this.textContent) {
+  	  article.classList.remove('hidden');
+  	}
+  }
+  this.classList.add('ui-tabs-active');
+}
