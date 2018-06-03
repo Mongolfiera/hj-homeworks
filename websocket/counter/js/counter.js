3855c1b6ab1da2e32a438e05dec879bb3a9dce29
Mongolfiera <elenadruganova@mail.ru>
2018-06-03 23:53:46 +0300

    Update counter.js

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,10 @@
 'use strict';
+const counter = document.querySelector('.counter');
+const errors = document.querySelector('output.errors');
+const connection = new WebSocket('wss://neto-api.herokuapp.com/counter'); 
+connection.addEventListener('message', event => {
+	const data = JSON.parse(event.data);
+	counter.textContent = data.connections;
+	errors.textContent = data.errors;
+}); 
+window.addEventListener('beforeunload', () => { connection.close(1000)});
