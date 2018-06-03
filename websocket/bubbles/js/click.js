113651b481d3ee1391cbf05f2131e46668e30383
Mongolfiera <elenadruganova@mail.ru>
2018-06-03 23:40:13 +0300

    Update click.js

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,10 @@
 'use strict';
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse'); 
+document.addEventListener('click', event => {
+  connection.send(JSON.stringify({
+	x: event.x,
+	y: event.y
+  }));
+});
+
+showBubbles(connection);
