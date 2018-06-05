c29102e554f635ad6743c72a5410dc5d794a49e8
Mongolfiera <elenadruganova@mail.ru>
2018-06-05 09:08:49 +0300

    Update socket.js

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -3,3 +3,4 @@ const connection = new WebSocket('wss://neto-api.herokuapp.com/draw');
 window.editor.addEventListener('update', () => {
   canvas.toBlob(pic => connection.send(pic));
 }); 
+window.addEventListener('beforeunload', () => connection.close(););
