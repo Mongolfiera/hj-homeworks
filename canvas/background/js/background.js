595aea2357e29ab893cf5e5fc5f51ee917c276c9
Mongolfiera <elenadruganova@mail.ru>
2018-06-09 00:26:09 +0300

    Update background.js

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -7,23 +7,86 @@ ctx.strokeStyle = '#ffffff';
 const curves = [];
 
 function getCircle(x, y, size) {
-	ctx.beginPath();
-	ctx.lineWidth = 5 * size;
-	ctx.arc(x, y, size * 6, 0, 2 * Math.PI);
-	ctx.stroke();
+  ctx.beginPath();
+  ctx.lineWidth = 5 * size;
+  ctx.arc(x, y, size * 6, 0, 2 * Math.PI);
+  ctx.stroke();
 }
 
 function getCross(x, y, size, angle) {
-	ctx.save();
-	ctx.beginPath();
-	ctx.lineWidth = 5 * size;
-	ctx.translate(x , y);
-	ctx.rotate(angle);
-	ctx.translate(-x , -y);
-	ctx.moveTo(x, y - size * 10);
-	ctx.lineTo(x, y + size * 10);
-	ctx.moveTo(x - size * 10, y); 
-	ctx.lineTo(x + size * 10, y);
-	ctx.stroke();
-	ctx.restore();
-} 
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 5 * size;
+  ctx.translate(x , y);
+  ctx.rotate(angle);
+  ctx.translate(-x , -y);
+  ctx.moveTo(x, y - size * 10);
+  ctx.lineTo(x, y + size * 10);
+  ctx.moveTo(x - size * 10, y); 
+  ctx.lineTo(x + size * 10, y);
+  ctx.stroke();
+  ctx.restore();
+}
+
+function randomValue(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getTimeFunction() {
+  const choose = Math.floor(randomValue(0, 2));
+  switch (choose) {
+    case 0:
+      return function nextPoint(x, y, time) {
+        return {
+          x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
+          y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
+        };
+      }
+    case 1:
+      return function nextPoint(x, y, time) {
+        return {
+          x: x + Math.sin((x + (time / 10)) / 100) * 5,
+          y: y + Math.sin((10 + x + (time / 10)) / 100) * 2
+        };
+      }
+  }
+}
+
+function createObjects() {
+  const num = Math.floor(randomValue(50, 201));
+  for (let i = 0; i < num; i++) {
+    const object = {};
+    object.size = Math.floor(randomValue(100, 601)) / 1000;
+    object.x = Math.floor(randomValue(0, canvas.width + 1));
+    object.y = Math.floor(randomValue(0, canvas.height + 1));
+    object.nextPoint = getTimeFunction();
+    if (i % 2) {
+      object.create = getCross;
+      object.velocity = Math.floor(randomValue(-200, 201)) / 1000;
+      object.angle = Math.floor(randomValue(0, 360));
+    } else {
+      object.create = getCircle;
+      object.angle = 0;
+    }
+    curves.push(object);
+  }
+}
+
+createObjects();
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  curves.forEach(object => {
+    const nextPoint = object.nextPoint(object.x, object.y, Date.now());
+    object.create(nextPoint.x, nextPoint.y, object.size, object.angle += object.velocity);
+  });
+}
+
+function tick(timestamp) {
+  if (!(Math.floor(timestamp) % 3)) {
+    animate();
+  }
+  requestAnimationFrame(tick);
+}
+
+tick();
