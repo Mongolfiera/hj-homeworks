a4768563c1f80b8b8c317cc7e50b35b1eb318e39
Mongolfiera <elenadruganova@mail.ru>
2018-06-18 01:10:34 +0300

    Update counter.js

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,26 @@
 'use strict';
+const counter = document.getElementById('counter');
+let count;
+
+document.addEventListener('DOMContentLoaded', () => {
+  count = (localStorage.getItem('counter')) ? parseInt(localStorage.getItem('counter')) : 0;
+  counter.textContent = count;
+});
+
+document.querySelector('.wrap-btns').addEventListener('click', updateCounter);
+
+function updateCounter(event) {
+  switch(event.target.id) {
+    case 'increment':
+      count++;
+      break;
+    case 'decrement':
+      if (count > 0) count--;
+      break;
+    case 'reset':
+      count = 0;
+      break;
+	}
+  counter.textContent = count;
+  localStorage.counter = count;
+}
